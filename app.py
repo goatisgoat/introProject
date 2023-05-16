4439f94083ae48b8b391d9722b8ab86d33c0bb20
Yujin Kim <cocodj0509@naver.com>
2023-05-16 20:57:31 +0900

    second Commit

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, request, render_template, jsonify
 app = Flask(__name__)
-
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 import certifi
 
 ca = certifi.where()
 
-client = MongoClient('주소를 입력해주세요', tlsCAFile=ca)
+client = MongoClient('mongodb+srv://sparta:test@cluster0.ucno5cd.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
+
+
 
 @app.route('/')
 def home():
@@ -15,7 +17,17 @@ def home():
 
 
 
+# 댓글 가져오기
+@app.route("/showComment", methods=["GET"])
+def comment_get():
+       teamComment_data = list(db.teamComment.find())
+       for i in range(len(teamComment_data)):
+        teamComment_data[i]['_id'] = str(teamComment_data[i]['_id'])
+       return jsonify({'result': teamComment_data })
 
+
+
+# 댓글 작성
 @app.route("/introPosting", methods=["POST"])
 def comment_post():
     comment_receive = request.form['comment_give']
@@ -34,32 +46,25 @@ def comment_post():
 
 
 
+#DELETE
+@app.route("/comment_delete", methods=["POST"])
+def comment_delete():
+    id_receive = request.form['id']
+    db.teamComment.delete_one({'_id': ObjectId(id_receive)})
+    return jsonify({'msg':"삭제 완료!"})
 
 
-# #DELETE
-# @app.route("/comment_delete", methods=["POST"])
-# def comment_delete():
-#     #db에 m_id가 int로 저장되어 있으니 str로 넘어오는 m_id를 int로 변경해준다.
-#     index_receive = request.form['_id']
-#     #m_id가 받아온 m_id인걸 찾아서 삭제한다.
-#     db.teamComment.delete_one({'index':index_receive})
-#     return jsonify({'msg':"삭제 완료!"})
-
-# 댓글 가져오기
-@app.route("/showComment", methods=["GET"])
-def comment_get():
-       teamComment_data = list(db.teamComment.find({},{'_id':False}))
-       return jsonify({'result': teamComment_data })
 
 
 
 # 좋아요
 @app.route("/fan/like", methods=["POST"]) 
 def fan_like():
-    num_receive = request.form['num_give']
-    like_receive = request.form['like_give']
+   #  num_receive = request.form['num_give']
+   #  like_receive = request.form['like_give']
+    id_receive = request.form['id_give']
 
-    db.teamComment.update_one({'num': int(num_receive) },{'$set': {'like' : int(like_receive) + 1 }})
+    db.teamComment.update_one({'_id': id_receive },{'$set': {'like' : int(like_receive) + 1 }})
     return jsonify({'msg':'좋아요'})
 
 if __name__ == '__main__':
--- static/index.js
+++ static/index.js
@@ -1,5 +1,36 @@
+// $(document).ready(function(){ })와 같음.
+$(function () {
+  listing();
+});
+
+
+// READ
+const listing = () => {
+  fetch("/showComment")
+    .then((res) => res.json())
+    .then((data) => {
+      let rows = data["result"];
+      $("#inputList").empty();
+      rows.forEach((a) => {
+        let comment = a['comment']
+        let like = a['like']
+        let num = a['num']
+        let id = a['_id']
+        console.log(id)
+        let temp_html = `<li>
+                        ${comment}<i id=${id} onclick='commentdelete(event)' class="fa-regular fa-square-minus"></i>
+                        <div class="like">   
+                        <p class="like-num">${like}</p>                             
+                        <p type="button" id=${id} onClick={(e)=>{clickLike(${like}, e)}} class="like-logo">🖤</p>
+                       </div>
+                      </li>`;
+        $("#inputList").append(temp_html);
+      });
+    });
+};
+
+// 댓글작성
 const posting = () => {
-  // let url = $("#url").val();
   let comment = $("#commentInput").val();
 
   let formData = new FormData();
@@ -14,52 +45,34 @@ const posting = () => {
     });
 };
 
-// const movieworld_delete = () => {
-//   let formData = new FormData();
-//   formData.append("index", index);
-//   fetch("/comment_delete", { method: "POST", body: formData })
-//     .then((res) => res.json())
-//     .then((data) => {
-//       alert(data["msg"]);
-//       //새로고침
-//       window.location.reload();
-//     });
-// };
-
-// $(document).ready(function(){ })와 같음.
-$(function () {
-  listing();
-});
-
-// READ
-const listing = () => {
-  fetch("/showComment")
+// 댓글삭제
+const commentdelete = (event) => {
+  let id = event.target.getAttribute('id');
+  let formData = new FormData();
+  formData.append("id", id);
+  fetch("/comment_delete", { method: "POST", body: formData })
     .then((res) => res.json())
     .then((data) => {
-      let rows = data["result"];
-      $("#inputList").empty();
-      rows.forEach((a) => {
-        let like = a['like']
-        let num = a['num']
-        let temp_html = `<li>
-                        ${a["comment"]}<i class="fa-regular fa-square-minus"></i>
-                        <div class="like">   
-                        <p class="like-num">${like}</p>                             
-                        <p type="button" onclick="clickLike(${num},${like})" class="like-logo">🖤</p>
-                       </div>
-                      </li>`;
-        $("#inputList").append(temp_html);
-      });
+      alert(data["msg"]);
+      //새로고침
+      window.location.reload();
     });
 };
 
-function clickLike(num, like) {
-  let receiveNum = num
-  let receiveLike = like
 
+
+// 좋아요
+const clickLike = (like, e) => {
+  let id = e.target.getAttribute('id');
+
+  console.log(id)
+
+  like++
   let formData = new FormData();
-  formData.append("num_give", receiveNum);
-  formData.append("like_give", receiveLike);
+  // formData.append("num_give", receiveNum);
+  // formData.append("like_give", like);
+  formData.append("id_give", id);
+  formData.append("like_give", like);
 
   fetch("/fan/like", { method: "POST", body: formData })
     .then((res) => res.json())
